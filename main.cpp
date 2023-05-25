0480cf1edbab4a127497f488a2e3fed9be6bcee8
Novak <novaldix@gmail.com>
2023-05-25 15:52:43 +0300

    16_6_4 fix functionality

--- main.cpp
+++ main.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <cctype>
 #include <algorithm> // any_of
-#include <vector>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
-enum class Notes { DO, RE, MI, FA, SO, LA, SI };
+vector<string> notesNames = { "DO", "RE", "MI", "FA", "SO", "LA", "SI" };
 
-// Получаем true если элемент `item` хоть раз встречается в диапазоне `range`
-bool isIncludes(const string &range, const char &item) {
+bool isIncludes(const char &item, const std::string &range) {
     return std::any_of(range.begin(),
                        range.end(),
                        [&item](const char &c) { return c == item; });
 }
 
-// Получаем char-символ в обозначенном диапазоне `range`
+std::string getJoinRange(const std::string &range) {
+    char const TERMINATOR = '.';
+    char const JOIN = ',';
+
+    std::string joinRange;
+    for (int i = 0; i < range.size(); ++i) {
+        joinRange += range[i];
+        joinRange += (i != range.size() - 1) ? JOIN : TERMINATOR;
+    }
+
+    return joinRange;
+}
+
 template<typename T>
-T getUserChar(string const &range) {
+T getUserChar(std::string const &range) {
     T input;
 
     while (true) {
         std::cin >> input;
         // Если введено значение из диапазона - сбрасываем
-        if (isIncludes(range, input)) break;
-        printf("Error. (%c) not in range (%s)\n", input, range.c_str());
+        if (isIncludes(input, range)) break;
+        printf("Error. Symbol %c not in range: %s Repeat: ", input, getJoinRange(range).c_str());
         // Сбрасываем коматозное состояние cin
         std::cin.clear();
         // Очищаем поток ввода
@@ -36,59 +46,52 @@ T getUserChar(string const &range) {
     return input;
 }
 
-// Получаем слово с параметрами:
-//  - не более определенной длины (`MAX_COUNT`),
-//  - с окончанием для ввода (`DOT`),
-//  - в указанном диапазоне `range`
-string getUserInput() {
-    char const DOT = '.';
-    size_t MAX_COUNT = 12;
-    string range = "0123456";
+string getUserInput(const string &range, bool isRepeatAllowed = false, size_t maxCount = 128) {
+    char const TERMINATOR = '.';
     string userInput;
     size_t curCount = 0;
 
-    range.append(1, DOT);
+    string constraints = range;
+    constraints.append(1, TERMINATOR);
 
     // to format: zu - size_t, s - string.c_str, c - char
-    printf("Enter up to (%zu) characters in the range (%s)\n", MAX_COUNT, range.c_str());
-    printf("Press (%c) to finish: ", DOT);
-    while(curCount < MAX_COUNT) {
-        char a = getUserChar<char>(range);
-        if (a == DOT) break;
-        userInput += a;
+    printf("Enter%s valid characters: %s ", (isRepeatAllowed ? "" : " unique"), getJoinRange(range).c_str());
+    printf("To complete - press '%c': ", TERMINATOR);
+    while (curCount < maxCount) {
+        char ch = getUserChar<char>(constraints);
+        if (ch == TERMINATOR) break;
+        if (!isRepeatAllowed && isIncludes(ch, userInput)) {
+            printf("'%c' is already there in %s. Enter another or press '%c': ", ch, userInput.c_str(),
+                   TERMINATOR);
+            continue;
+        }
+        userInput += ch;
         ++curCount;
     }
 
     return userInput;
 }
 
-// Получаем соответствующую enum аббревиатуру
-string enumToString(Notes &e) {
-    std::vector<string> inner = { "DO", "RE", "MI", "FA", "SO", "LA", "SI"};
-    return inner[(int)e];
-}
-
-void printNotesCollection(string const &userInputString) {
-    std::vector<Notes> notesCollection;
-
-    // Создаем на основе отдельных символов, вектор из enum
+// Создаем на основе отдельных символов, вектор из enum
+void setNotesCollection(string const &userInputString, vector<string> &notesCollection) {
     for (char const &ch : userInputString) {
-        auto note = static_cast<Notes>(ch - '0');
+        auto note = notesNames[(ch - '0')];
         notesCollection.push_back(note);
     }
+}
 
+void printNotesCollection(vector<string> const &notesCollection) {
     cout << "Song notes:: " << endl;
-    for (int i = 0; i < userInputString.length(); ++i) {
-        auto currentNote = notesCollection[i];
-        cout << enumToString(currentNote) << ((i + 1) % 3 ? " " : "\n");
+    for (int i = 0; i < notesCollection.size(); ++i) {
+        cout << notesCollection[i] << ((i + 1) % 3 ? " " : "\n");
     }
 }
 
 int main() {
-    string store = getUserInput();
+    std::vector<string> notesCollection;
+    string userInput = getUserInput("0123456", true, 12);
+    cout << "Values: " << userInput << endl;
 
-    cout << "Values: " << store << endl;
-
-    printNotesCollection(store);
+    setNotesCollection(userInput, notesCollection);
+    printNotesCollection(notesCollection);
 }
-
